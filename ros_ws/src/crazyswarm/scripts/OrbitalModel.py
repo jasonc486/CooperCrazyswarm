c1a98d63d613dda31157140c121ab727e526b2fe
jasonc486 <jason.chen@cooper.edu>
2022-07-22 21:28:58 -0400

    Working Trials in Orbital Model

--- ros_ws/src/crazyswarm/scripts/OrbitalModel.py
+++ ros_ws/src/crazyswarm/scripts/OrbitalModel.py
@@ -6,14 +6,29 @@ from pycrazyswarm import *
 
 Z = 0.5
 sleepRate = 30
+Trials = 5
+i = 0
 
+def positionChecker(initialPos, currentPos):
+    return (((initialPos[0] - 0.01) < currentPos[0]) and (currentPos[0] < (initialPos[0] + 0.01))) 
 
 def goCircle(timeHelper, cf, totalTime, radius, kPosition):
         startTime = timeHelper.time()
         pos = cf.position()
+        tempPos = cf.position()
+        tempBool = True
+        trialCounter = 0
         startPos = cf.initialPosition + np.array([0, 0, Z])
         center_circle = startPos - np.array([radius, 0, 0])
-        while True:
+        print(pos)
+        keepGoing = True
+        while keepGoing:
+            if positionChecker(pos, cf.position()) and not positionChecker(pos, tempPos):
+                print("This works")
+                trialCounter += 1
+                if trialCounter == Trials:
+                    keepGoing = False
+            tempPos = cf.position()
             time = timeHelper.time() - startTime
             omega = 2 * np.pi / totalTime
             vx = -radius * omega * np.sin(omega * time)  
@@ -25,6 +40,7 @@ def goCircle(timeHelper, cf, totalTime, radius, kPosition):
                                          kPosition * errorX),
                                 yawRate=0)
             timeHelper.sleepForRate(sleepRate)
+            #print(cf.position())
 
 
 if __name__ == "__main__":
@@ -35,3 +51,5 @@ if __name__ == "__main__":
     allcfs.takeoff(targetHeight=Z, duration=1.0+Z)
     timeHelper.sleep(2 + Z)
     goCircle(timeHelper, allcfs.crazyflies[0], totalTime=4, radius=1, kPosition=1)
+    allcfs.land(targetHeight=0.06, duration=2.0)
+    timeHelper.sleep(3.0)
